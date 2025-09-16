ac5d2b368e928674c31a95847b7ab6323caf3b6f
Qwerty (Vítězslav Ackermann Ferko) <qwerty@qwerty.xyz>
2025-09-16 08:10:20 +0200

    Configure project and Install packages with pnpm

--- eslint.config.js
+++ eslint.config.js
@@ -4,20 +4,59 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import imports from 'eslint-plugin-import'
 
 export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
+	globalIgnores(['dist']),
+	{
+		files: ['**/*.{ts,tsx}'],
+		extends: [
+			js.configs.recommended,
+			tseslint.configs.recommended,
+			reactHooks.configs['recommended-latest'],
+			reactRefresh.configs.vite,
+		],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser,
+		},
+		plugins: {
+			import: imports,
+		},
+		rules: {
+			'semi': ['error', 'never'],
+			'quotes': ['error', 'single'],
+			"import/order": ["error", {
+				"newlines-between": "always",
+				"alphabetize": { "order": "asc", "caseInsensitive": true },
+				"groups": ["builtin", "external", "internal", ["parent", "sibling", "index"]],
+				"pathGroups": [
+					{
+						"pattern": "react",
+						"group": "external",
+						"position": "before",
+					},
+					{
+						"pattern": "{@/ui,@/ui/**}",
+						"group": "internal",
+						"position": "after",
+					},
+					{
+						"pattern": "@/**",
+						"group": "internal",
+					},
+					{
+						"pattern": "../**",
+						"group": "parent",
+						"position": "before",
+					},
+					{
+						"pattern": "{./,.}",
+						"group": "index",
+						"position": "after",
+					},
+				],
+			}],
+		},
+	},
 ])
